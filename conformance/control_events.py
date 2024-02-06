bfaeb3b9c8ba21c2b7401d73db7e868e5f77e252
Bob Jacobsen <jacobsen@mac.com>
2024-02-06 13:14:11 -0500

    rest of event tests

--- conformance/control_events.py
+++ conformance/control_events.py
@@ -6,12 +6,16 @@ Simple runner for message network tests
 
 import check_ev10_ida
 import check_ev20_idg
+import check_ev30_ip
+import check_ev40_ic
 
 def prompt() :
     print("\nEvent Exchange Standard testing")
     print(" 0 Run all in sequence")
     print(" 1 Identify Event Addressed testing")
     print(" 2 Identify Event Global testing")
+    print(" 3 Identify Producer testing")
+    print(" 4 Identify Consumer testing")
     print("  ")
     print(" q go back")
 
@@ -24,10 +28,18 @@ def testAll() :
     print("\nIdentify Event Global testing")
     result += check_ev20_idg.test()
 
+    print("\nIdentify Producer testing")
+    result += check_ev30_ip.test()
+
+    print("\nIdentify Consumer testing")
+    result += check_ev40_ic.test()
+
     if result == 0 :
         print("\nSuccess - all passed")
     else:
         print("\nAt least one test failed")
+
+    return result
     
 def main() :
     '''
@@ -43,6 +55,12 @@ def main() :
             case "2" :
                 print("\nIdentify Event Global testing")
                 check_ev20_idg.test()
+            case "3" : 
+                print("\nIdentify Producer testing")
+                check_ev30_ip.test()
+            case "4" :
+                print("\nIdentify Consumer testing")
+                check_ev40_ic.test()
            
             case  "0" :
                 testAll()
