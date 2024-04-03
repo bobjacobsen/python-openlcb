740f3fc74853ece4f71d0f9405205aed66e00dc7
Poikilos <7557867+Poikilos@users.noreply.github.com>
2024-04-03 12:12:51 -0400

    Fix undefined nodeId in receivedPart, and add missing substitution (change [] to {}). FIXME: undefined name "messagePart". Use PEP8 more (but ignore E501 line too long in some cases). FIXME: Consider deleting openlcb/tcplink/__init__.py then in a separate commit (So git can track the rename operation), rename openlcb/tcplink/tcplink.py to openlcb/tcplink/__init__.py.

--- openlcb/tcplink/tcplink.py
+++ openlcb/tcplink/tcplink.py
@@ -19,24 +19,40 @@ from openlcb.nodeid import NodeID
 import logging
 import time
 
-class TcpLink(LinkLayer):
 
-    def __init__(self, localNodeID):  # a NodeID
+class TcpLink(LinkLayer):
+    """A TCP link layer.
+
+    Attributes:
+        accumulatedData (list): input accumulated until an entire message is
+            present.
+
+    Args:
+        localNodeID (NodeID): The node ID of the Configuration Tool or other
+            software-defined node connecting to the LCC network via TCP.
+    """
+
+    def __init__(self, localNodeID):
+        # See class docstring for argument(s) and attributes.
         self.localNodeID = localNodeID
         self.linkCall = None
         self.accumulatedParts = {}
         self.nextInternallyAssignedNodeID = 1
-        self.accumulatedData = []  # input accumulated until an entire message is present
+        self.accumulatedData = []
 
-    def linkPhysicalLayer(self, lpl):  # usually a socket connection send() method
-        self.linkCall = lpl
- 
- 
-    def receiveListener(self, inputData):  # [] input
+    def linkPhysicalLayer(self, lpl):
+        """Register the handler for when the layer is up.
+
+        Args:
+            lpl (function): A handler that accepts a bytes object, usually a
+                socket connection send() method.
         """
-        Receives bytes from lower level and 
-        accumulates them into individual message parts.
-        
+        self.linkCall = lpl
+
+    def receiveListener(self, inputData):  # [] input
+        """Receives bytes from lower level
+        and accumulates them into individual message parts.
+
         Args:
             inputData ([int]) : next chunk of the input stream
         """
@@ -44,61 +60,66 @@ class TcpLink(LinkLayer):
         # Now check it if has one or more complete message.
         while len(self.accumulatedData) > 0 :
             # first, see if entire prefix is present
-            if len(self.accumulatedData) < 17 : # 2+3+6+6
+            if len(self.accumulatedData) < 17 :  # 2+3+6+6
                 # not yet, wait for more
                 return
-            flags = (self.accumulatedData[0]<<8) | self.accumulatedData[1]
-            length =(self.accumulatedData[2]<<16) |(self.accumulatedData[3]<<8) | self.accumulatedData[4]
+            flags = (self.accumulatedData[0] << 8) | self.accumulatedData[1]
+            length = (self.accumulatedData[2] << 16) | (self.accumulatedData[3] << 8) | self.accumulatedData[4]  # noqa: E501
             # check if entire message (part) is present
             if len(self.accumulatedData) < 5+length :
                 # not yet, wait for more
                 return
-        
+
             # Check for message indicated bit
             if (self.accumulatedData[0] & 0x80) == 0x80:
                 # we have a message (part)!  Forward for further processing
-                self.receivedPart(self.accumulatedData[:5+length], flags, length)
+                self.receivedPart(self.accumulatedData[:5+length], flags,
+                                  length)
             else:
                 # We don't have definitions for link control messages
                 # so log and ignore
-                logging.info("Found a link control message with flags 0x{:04X} length {}, ignoring"
-                                .format(flags, length))
+                logging.info(
+                    "Found a link control message"
+                    " with flags 0x{:04X} length {}, ignoring"
+                    .format(flags, length)
+                )
             # drop that message (part)
             self.accumulatedData = self.accumulatedData[5+length:]
             # and repeat
 
-    def receivedPart(self, messagePart, flags, length): # messagePart is raw message data
-        """
-        Receives message parts from receiveListener and groups them into 
-        single OpenLCB messages as needed
-        
+    def receivedPart(self, messagePart, flags, length):
+        """Receives message parts from receiveListener
+        and groups them into single OpenLCB messages as needed
+
         Args:
-            messagePart ([int]) : a single TCP-level meesage, which 
-                may include all or part of a single OpenLCB message
+            messagePart (int) : Raw message data. A single TCP-level message,
+                which may include all or part of a single OpenLCB message.
         """
         # set the source NodeID from the data
         gatewayNodeID = NodeID(messagePart[5:11])
-        
+
         # handle simplest case first - complete message
-        if (flags& 0x00C0) == 0x00000 :
+        if (flags & 0x00C0) == 0x00000 :
             self.forwardMessage(messagePart[17:], gatewayNodeID)
             return
-        
-        # need to accumulate - can be first, middle or last, but not entire message
+
+        # need to accumulate
+        # - can be first, middle or last, but not entire message
         key = gatewayNodeID   # do we need to have the capture time in here?
-        if (flags & 0x00C0) == 0x040 : # first
-            # check for error 
+        if (flags & 0x00C0) == 0x040 :  # first
+            # check for error
             if self.accumulatedParts.get(key) is not None :
                 # this was a first, but shouldn't have been
-                logging.warn("Found a first part from [] while already accumulating"
-                                .format(nodeId))
+                logging.warn("Found a first part from {}"
+                             " while already accumulating"
+                             "".format(gatewayNodeID))
                 # start over
             # start accumulation
             self.accumulatedParts[key] = []
         # accumulate next part
         self.accumulatedParts[key].extend(messagePart[17:])
         # is the accumulation complete?
-        if (flags & 0x00C0) == 0x0080 : # first
+        if (flags & 0x00C0) == 0x0080 :  # first
             # yes, forward to upper layer
             messageBytes = self.accumulatedParts[key]
             self.forwardMessage(messageBytes, gatewayNodeID)
@@ -106,14 +127,14 @@ class TcpLink(LinkLayer):
             del self.accumulatedParts[key]
         # wait for next part
         return
-            
-    def forwardMessage(self, messageBytes, gatewayNodeID) : # not sure why gatewayNodeID useful here...
+
+    def forwardMessage(self, messageBytes, gatewayNodeID) :  # not sure why gatewayNodeID useful here...  # noqa: E501
         """
         Receives single message from receivedPart, converts
         it in a Message object, and forwards to listeners
-        
+
         Args:
-            messageBytes ([int]) : the bytes making up a 
+            messageBytes ([int]) : the bytes making up a
                 single OpenLCB message, starting with the MTI
         """
         # extract MTI
@@ -130,11 +151,11 @@ class TcpLink(LinkLayer):
         message = Message(mti, sourceNodeID, destNodeID, data)
         # forward to listeners
         self.fireListeners(message)
-    
+
     def linkUp(self):
         """
         Link started,  notify upper layers
-        """ 
+        """
         msg = Message(MTI.Link_Layer_Up, NodeID(0), None, [])
         self.fireListeners(msg)
 
@@ -148,31 +169,31 @@ class TcpLink(LinkLayer):
     def linkDown(self):
         """
         Link dropped,  notify upper layers
-        """ 
+        """
         msg = Message(MTI.Link_Layer_Down, NodeID(0), None, [])
         self.fireListeners(msg)
 
     def sendMessage(self, message):
         """
-        The message level calls this with an OpenLCB 
+        The message level calls this with an OpenLCB
         message.  That is then converted to a byte
         stream and forwarded to the TCP socket layer.
         """
-    
+
         mti = message.mti
 
-        outputBytes = [0x80, 0x00] # flags
-        
+        outputBytes = [0x80, 0x00]  # flags
+
         length = 12+2+6+len(message.data)
         if mti.addressPresent() : length = length+6
 
         l0 = (length & 0xFF0000) >> 16
         l1 = (length & 0xFF00) >> 8
         l2 = (length & 0xFF)
-        outputBytes.extend([l0,l1,l2])
-        
+        outputBytes.extend([l0, l1, l2])
+
         outputBytes.extend(self.localNodeID.toArray())
-        
+
         t = round(time.time() * 1000)
         t0 = (t & 0xFF0000000000) >> 40
         t1 = (t & 0xFF00000000) >> 32
@@ -180,20 +201,17 @@ class TcpLink(LinkLayer):
         t3 = (t & 0xFF0000) >> 16
         t4 = (t & 0xFF00) >> 8
         t5 = (t & 0xFF)
-        outputBytes.extend([t0,t1,t2,t3,t4,t5])
-        
+        outputBytes.extend([t0, t1, t2, t3, t4, t5])
+
         m0 = (mti.value & 0xFF00) >> 8
         m1 = (mti.value & 0xFF)
         outputBytes.extend([m0, m1])
-        
-        outputBytes.extend(message.source.toArray())
-        
-        if mti.addressPresent() : outputBytes.extend(message.destination.toArray())
-        
-        outputBytes.extend(message.data)
-        
-        self.linkCall(outputBytes)
-       
-        
-        
 
+        outputBytes.extend(message.source.toArray())
+
+        if mti.addressPresent() :
+            outputBytes.extend(message.destination.toArray())
+
+        outputBytes.extend(message.data)
+
+        self.linkCall(outputBytes)
