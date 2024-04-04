bobjacobsen/python-openlcb

commit cf4a3759a8c2248b51666757aaba6813cbbe864e

Add docstrings, and make some existing ones Google Style such as for IDE hinting and Sphinx documentation generation. Use PEP8 more (mostly spacing, though several spacing rules are ignored by flake8 --ignore=... in an earlier commit to python-openlcb.code-workspace. flake8 and pylance [via ruff] discovered some legitimate bugs noted in earlier commits, so reducing style warnings helps narrow them down).

openlcb/pip.py
@@ -53,14 +53,15 @@ class PIP(Enum):
             retval.append(pip.name.replace("_", " ").title())
         return retval
 
-    def setContentsFromInt(input):
-        """Get a set of contents from a single numeric input
+    def setContentsFromInt(bitmask):
+        """Get a set of contents from a single numeric bitmask
 
         Args:
-            input (_type_): _description_
+            bitmask (int): A single number that is the sum of 1 or more
+                protocol bits.
 
         Returns:
-            set: _description_
+            set (PIP): The set of protocol bits derived from the bitmask.
         """
         retVal = []
         for val in PIP.list():
@@ -72,10 +73,10 @@ class PIP(Enum):
         """set contents from a list of numeric inputs
 
         Args:
-            raw (_type_): _description_
+            raw (Union[bytes,list[int]]): a list of 1-byte values
 
         Returns:
-            _type_: _description_
+            set (PIP): The set of protocol bits derived from the raw data.
         """
         data = 0
         if (len(raw) > 0):

example_cdi_access.py
@@ -13,7 +13,9 @@ host|host:port            (optional) Set the address (or using a colon,
 
 from openlcb.canbus.tcpsocket import TcpSocket
 
-from openlcb.canbus.canphysicallayergridconnect import CanPhysicalLayerGridConnect
+from openlcb.canbus.canphysicallayergridconnect import (
+    CanPhysicalLayerGridConnect,
+)
 from openlcb.canbus.canlink import CanLink
 from openlcb.nodeid import NodeID
 from openlcb.datagramservice import (
@@ -28,7 +30,7 @@ from openlcb.memoryservice import (
 host = "192.168.16.212"
 port = 12021
 localNodeID = "05.01.01.01.03.01"
-#farNodeID = "09.00.99.03.00.35"
+# farNodeID = "09.00.99.03.00.35"
 farNodeID = "02.01.57.00.04.9C"
 
 # region same code as other examples
@@ -68,11 +70,12 @@ s = TcpSocket()
 s.connect(host, port)
 
 
-#print("RR, SR are raw socket interface receive and send;"
+# print("RR, SR are raw socket interface receive and send;"
 #      " RL, SL are link interface; RM, SM are message interface")
 
+
 def sendToSocket(string):
-    #print("      SR: {}".format(string.strip()))
+    # print("      SR: {}".format(string.strip()))
     s.send(string)
 
 
@@ -80,21 +83,23 @@ def printFrame(frame):
     # print("   RL: {}".format(frame))
     pass
 
+
 def printMessage(message):
-    #print("RM: {} from {}".format(message, message.source))
+    # print("RM: {} from {}".format(message, message.source))
     pass
 
+
 def printDatagram(memo):
-    """create a call-back to print datagram contents when received
+    """A call-back for when datagrams received
 
     Args:
-        memo (_type_): _description_
+        DatagramReadMemo: The datagram object
 
     Returns:
-        bool: Always False (True would mean we sent a reply to this datagram,
-            but let MemoryService do that).
+        bool: Always False (True would mean we sent a reply to the datagram,
+            but let the MemoryService do that).
     """
-    #print("Datagram receive call back: {}".format(memo.data))
+    # print("Datagram receive call back: {}".format(memo.data))
     return False
 
 
@@ -113,25 +118,26 @@ datagramService.registerDatagramReceivedListener(printDatagram)
 memoryService = MemoryService(datagramService)
 
 
-
 # accumulate the CDI information
 resultingCDI = []
 
-# Invoked when the memory read successfully returns, 
-# this queues a new read until the entire CDI has been
-# returned.  At that point, it invokes the XML processing below.
+
 def memoryReadSuccess(memo):
     """createcallbacks to get results of memory read
+    Invoked when the memory read successfully returns,
+    this queues a new read until the entire CDI has been
+    returned.  At that point, it invokes the XML processing below.
+
 
     Args:
         memo (_type_): _description_
     """
-    #print("successful memory read: {}".format(memo.data))
-    
+    # print("successful memory read: {}".format(memo.data))
+
     global resultingCDI
 
     # is this done?
-    if len(memo.data) == 64 and not 0 in memo.data:
+    if len(memo.data) == 64 and 0 not in memo.data:
         # save content
         resultingCDI += memo.data
         # update the address
@@ -140,7 +146,7 @@ def memoryReadSuccess(memo):
         memoryService.requestMemoryRead(memo)
     else :
         # and we're done!
-        # save content 
+        # save content
         resultingCDI += memo.data
         # concert resultingCDI to a string up to 1st zero
         cdiString = ""
@@ -151,17 +157,19 @@ def memoryReadSuccess(memo):
 
         # and process that
         processXML(cdiString)
-        
+
         # done
-        
+
+
 def memoryReadFail(memo):
     print("memory read failed: {}".format(memo.data))
 
+
 #######################
 # The XML parsing section.
-# 
+#
 # This creates a handler object that just prints
-# information as it's presented. 
+# information as it's presented.
 #
 # Since `characters` can be called multiple times
 # in a row, we buffer up the characters until the `endElement`
@@ -169,8 +177,9 @@ def memoryReadFail(memo):
 
 import xml.sax
 
-# define XML SAX callbacks in a handler object
+
 class MyHandler(xml.sax.handler.ContentHandler):
+    """XML SAX callbacks in a handler object"""
     def __init__(self):
         self._charBuffer = []
 
@@ -192,17 +201,24 @@ class MyHandler(xml.sax.handler.ContentHandler):
     def characters(self, data):
         self._charBuffer.append(data)
 
+
 handler = MyHandler()
 
-# process the XML and invoke callbacks
+
 def processXML(content) :
+    """process the XML and invoke callbacks
+
+    Args:
+        content (_type_): _description_
+    """
     xml.sax.parseString(content, handler)
     print("\nParser done")
+
 
 #######################
 
 # have the socket layer report up to bring the link layer up and get an alias
-#print("      SL : link up")
+# print("      SL : link up")
 canPhysicalLayerGridConnect.physicalLayerUp()
 
 
@@ -228,6 +244,6 @@ thread.start()
 # process resulting activity
 while True:
     received = s.receive()
-    #print("      RR: {}".format(received.strip()))
+    # print("      RR: {}".format(received.strip()))
     # pass to link processor
     canPhysicalLayerGridConnect.receiveString(received)

example_datagram_transfer.py
@@ -12,7 +12,9 @@ host|host:port            (optional) Set the address (or using a colon,
 import threading
 
 from openlcb.canbus.tcpsocket import TcpSocket
-from openlcb.canbus.canphysicallayergridconnect import CanPhysicalLayerGridConnect
+from openlcb.canbus.canphysicallayergridconnect import (
+    CanPhysicalLayerGridConnect,
+)
 from openlcb.canbus.canlink import CanLink
 from openlcb.nodeid import NodeID
 from openlcb.datagramservice import (
@@ -101,7 +103,7 @@ def datagramReceiver(memo):
     """A call-back for when datagrams received
 
     Args:
-        _type_: _description_
+        DatagramReadMemo: The datagram object
 
     Returns:
         bool: Always True (means we sent the reply to this datagram)

example_frame_interface.py
@@ -12,7 +12,9 @@ host|host:port            (optional) Set the address (or using a colon,
 '''
 
 from openlcb.canbus.tcpsocket import TcpSocket
-from openlcb.canbus.canphysicallayergridconnect import CanPhysicalLayerGridConnect
+from openlcb.canbus.canphysicallayergridconnect import (
+    CanPhysicalLayerGridConnect,
+)
 from openlcb.canbus.canframe import CanFrame
 from openlcb.canbus.controlframe import ControlFrame
 

example_memory_transfer.py
@@ -13,7 +13,9 @@ host|host:port            (optional) Set the address (or using a colon,
 
 from openlcb.canbus.tcpsocket import TcpSocket
 
-from openlcb.canbus.canphysicallayergridconnect import CanPhysicalLayerGridConnect
+from openlcb.canbus.canphysicallayergridconnect import (
+    CanPhysicalLayerGridConnect,
+)
 from openlcb.canbus.canlink import CanLink
 from openlcb.nodeid import NodeID
 from openlcb.datagramservice import (
@@ -102,7 +104,7 @@ def printDatagram(memo):
     """create a call-back to print datagram contents when received
 
     Args:
-        memo (_type_): _description_
+        memo (DatagramReadMemo): The datagram received
 
     Returns:
         bool: Always False (True would mean we sent a reply to this datagram,
@@ -121,7 +123,7 @@ def memoryReadSuccess(memo):
     """createcallbacks to get results of memory read
 
     Args:
-        memo (_type_): _description_
+        memo (MemoryReadMemo): Event that was generated.
     """
     print("successful memory read: {}".format(memo.data))
 

example_message_interface.py
@@ -13,7 +13,9 @@ host|host:port            (optional) Set the address (or using a colon,
 '''
 from openlcb.canbus.tcpsocket import TcpSocket
 
-from openlcb.canbus.canphysicallayergridconnect import CanPhysicalLayerGridConnect
+from openlcb.canbus.canphysicallayergridconnect import (
+    CanPhysicalLayerGridConnect,
+)
 from openlcb.canbus.canlink import CanLink
 from openlcb.nodeid import NodeID
 from openlcb.message import Message

example_node_implementation.py
@@ -11,7 +11,9 @@ host|host:port            (optional) Set the address (or using a colon,
 '''
 from openlcb.canbus.tcpsocket import TcpSocket
 
-from openlcb.canbus.canphysicallayergridconnect import CanPhysicalLayerGridConnect
+from openlcb.canbus.canphysicallayergridconnect import (
+    CanPhysicalLayerGridConnect,
+)
 from openlcb.canbus.canlink import CanLink
 from openlcb.nodeid import NodeID
 from openlcb.datagramservice import DatagramService
@@ -99,7 +101,7 @@ def printDatagram(memo):
     """create a call-back to print datagram contents when received
 
     Args:
-        memo (_type_): _description_
+        memo (DatagramReadMemo): The datagram received
 
     Returns:
         bool: Always False (True would mean we sent a reply to the datagram,
@@ -136,11 +138,21 @@ localNode = Node(
 localNodeProcessor = LocalNodeProcessor(canLink, localNode)
 canLink.registerMessageReceivedListener(localNodeProcessor.process)
 
-# create a listener to identify connected nodes
+
 def displayOtherNodeIds(message) :
+    """Listener to identify connected nodes
+
+    Args:
+        message (_type_): _description_
+    """
+    print("[displayOtherNodeIds] type(message): {}"
+          "".format(type(message).__name__))
     if message.mti == MTI.Verified_NodeID :
         print("Detected farNodeID is {}".format(message.source))
+
+
 canLink.registerMessageReceivedListener(displayOtherNodeIds)
+
 
 #######################
 

example_remote_nodes.py
@@ -11,10 +11,12 @@ host|host:port            (optional) Set the address (or using a colon,
                           address and port.
 '''
 
-from openlcb.canbus.canphysicallayergridconnect import CanPhysicalLayerGridConnect
-from openlcb.canbus.canframe import CanFrame
+from openlcb.canbus.canphysicallayergridconnect import (
+    CanPhysicalLayerGridConnect,
+)
+# from openlcb.canbus.canframe import CanFrame
 from openlcb.canbus.canlink import CanLink
-from openlcb.canbus.controlframe import ControlFrame
+# from openlcb.canbus.controlframe import ControlFrame
 from openlcb.canbus.tcpsocket import TcpSocket
 
 from openlcb.node import Node
@@ -39,8 +41,10 @@ timeout = 0.5
 
 # region same code as other examples
 
+
 def usage():
     print(__doc__, file=sys.stderr)
+
 
 if __name__ == "__main__":
     # global host  # only necessary if this is moved to a main/other function
@@ -73,21 +77,27 @@ s.connect(host, port)
 
 
 if trace :
-    print("RR, SR are raw socket interface receive and send; RL, SL are link (frame) interface")
+    print("RR, SR are raw socket interface receive and send;"
+          " RL, SL are link (frame) interface")
+
 
 def sendToSocket(string) :
     if trace : print("   SR: "+string.strip())
     s.send(string)
 
-def receiveFrame(frame) : 
-    if trace: print("RL: "+str(frame) )
- 
+
+def receiveFrame(frame) :
+    if trace: print("RL: "+str(frame))
+
+
 canPhysicalLayerGridConnect = CanPhysicalLayerGridConnect(sendToSocket)
 canPhysicalLayerGridConnect.registerFrameReceivedListener(receiveFrame)
+
 
 def printMessage(msg):
     if trace: print("RM: {} from {}".format(msg, msg.source))
     readQueue.put(msg)
+
 
 canLink = CanLink(NodeID(localNodeID))
 canLink.linkPhysicalLayer(canPhysicalLayerGridConnect)
@@ -110,13 +120,16 @@ canLink.registerMessageReceivedListener(localNodeProcessor.process)
 remoteNodeStore = RemoteNodeStore(NodeID(localNodeID))
 remoteNodeProcessor = RemoteNodeProcessor(canLink)
 remoteNodeStore.processors = [remoteNodeProcessor]
-canLink.registerMessageReceivedListener(remoteNodeStore.processMessageFromLinkLayer)
+canLink.registerMessageReceivedListener(
+    remoteNodeStore.processMessageFromLinkLayer
+)
 
 
 readQueue = Queue()
 
-# put the read on a separate thread
+
 def receiveLoop() :
+    """put the read on a separate thread"""
     # bring the CAN level up
     if trace : print("      SL : link up")
     canPhysicalLayerGridConnect.physicalLayerUp()
@@ -125,26 +138,41 @@ def receiveLoop() :
         if trace : print("   RR: "+input.strip())
         # pass to link processor
         canPhysicalLayerGridConnect.receiveString(input)
-import threading
+
+
+import threading  # noqa E402
 thread = threading.Thread(daemon=True, target=receiveLoop)
 
-# define a routine for checking tests
+
 def result(arg1, arg2=None, arg3=None, result=True) :
-    # returns True if OK, False if failed
-    # If arg1 and arg2 provided
-    #   compare those, and fail if not equal; arg3 is then message
-    # If only arg1, report it and return result for fail value
+    """Check and report on test results.
+
+    Args:
+        arg1: Any value.
+        arg2: value to compare to arg1. Defaults to None.
+        arg3: fail if arg1 not equal to arg1; arg3 is then message. Defaults to
+            None.
+        result (bool, optional): _description_. Defaults to True.
+
+    Raises:
+        ValueError: If only arg1 was provided (undefined behavior--in other
+            words, test itself is wrong not the data).
+
+    Returns:
+        bool: True if OK, False if failed
+    """
     if arg2 is not None :
         if arg1 == arg2 :
             # OK
             print(arg1)
             return True
         else :
-            print("{} does not equal {}, FAIL".format(arg1, arg2))
+            raise ValueError("{} does not equal {}, FAIL".format(arg1, arg2))
             return False
     else:
         print(arg1)
         return result
+
 
 # start the process
 thread.start()
@@ -156,7 +184,7 @@ while True :
         received = readQueue.get(True, timeout)
         if trace : print("received: ", received)
     except Empty:
-         break
+        break
 
 # send an VerifyNodes message to provoke response
 print("\nSend Verify NodeID Number Global\n")
@@ -170,12 +198,13 @@ while True :
         received = readQueue.get(True, timeout)
         if trace : print("received: ", received)
     except Empty:
-         break
+        break
 
 # print the resulting node store contents
 print("\nDiscovered nodes:")
 
 for node in remoteNodeStore.asArray() :
-    print(node, node.snip.manufacturerName, "/", node.snip.userProvidedNodeName)
+    print(node, node.snip.manufacturerName, "/",
+          node.snip.userProvidedNodeName)
 
 # this ends here, which takes the local node offline

commit 44250f89c99401322637e51265d993d79bc31658

Fix a bug from a renamed variable (bug introduced in cf4a375 which tried to fix W0622 redefined builtin "input"). Clarify related docstrings.

openlcb/pip.py
@@ -57,15 +57,16 @@ class PIP(Enum):
         """Get a set of contents from a single numeric bitmask
 
         Args:
-            bitmask (int): A single number that is the sum of 1 or more
+            bitmask (int): A single number that is the sum of any number of
                 protocol bits.
 
         Returns:
-            set (PIP): The set of protocol bits derived from the bitmask.
+            set (PIP): The set of protocol bits (bitmasks where 1 bit is on in
+                each) derived from the bitmask.
         """
         retVal = []
         for val in PIP.list():
-            if (val.value & input != 0):
+            if (val.value & bitmask != 0):
                 retVal.append(val)
         return set(retVal)
 