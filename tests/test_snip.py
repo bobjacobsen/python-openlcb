392d3dbbe350fbf3c1347afb38948026bbb66cb9
Jacob Gustafson <7557867+Poikilos@users.noreply.github.com>
2025-01-11 14:31:29 -0500

    Note alternate Cyrillic spelling of Dmitry. Rename previous file for better sorting. Related to #55.

--- tests/test_snip.py
+++ tests/test_snip.py
@@ -152,7 +152,8 @@ class TestSnipClass(unittest.TestCase):
         s.userProvidedDescription = "4EF"
 
         s.updateSnipDataFromStrings()
-        self.assertEqual(s.getStringN(4), "Дмитрий")  # Cyrillic spelling of the name Dmitry. This string should appear as 7 Cyrillic characters like Dmitry-Cyrillic.png in doc (14 bytes in a hex editor), otherwise your editor does not support utf-8 and editing this file with it could break it.
+        self.assertEqual(s.getStringN(4), "Дмитрий")  # Cyrillic spelling of the name Dmitry. This string should appear as 7 Cyrillic characters like Cyrillic-demo-Dmitry.png in doc (14 bytes in a hex editor), otherwise your editor does not support utf-8 and editing this file with it could break it.
+        # TODO: Russian version is Дми́трий according to <https://en.wikipedia.org/wiki/Dmitry>. See Cyrillic-demo-Dmitry-Russian.png in doc.
 
     def testName(self):
         s = SNIP()  # init to all zeros
